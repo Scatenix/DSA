d54f2762a0b70344b96f33f12bcd2d1e5dca1d4d
Scatenix <38569240+Scatenix@users.noreply.github.com>
2025-02-22 21:56:14 +0100

    Added idea/todo comment for hashMap

--- datastructures/hashMap/hashMap.go
+++ datastructures/hashMap/hashMap.go
@@ -6,9 +6,8 @@ import "dsa/algorithms/hash"
 Some ideas for my HashMap
 
 Dynamic resizing:
-- When resizing, check size. if size n is 2^32 then switch to a hash function which outputs 64 Bit
-- Resize perhaps if a linked list has a size of >= 5 ? If we assume to have a hash function with good distribution, this should catch if we are overflowing.
-	-> resize to size n + n * 10% (or something like that) and perhaps if n < 100, resize to n * 2
+	Resizing is typically done by doubling the size of the underlying array and recalculating every key.
+	TODO: Think about adding a retrival of all nodes per linked list as function within the linked list itself or do it in this hashMap with node traversal.
 */
 
 func main() {
